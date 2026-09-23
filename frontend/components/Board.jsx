8df76a6a693810eba02711dce6dbcfeaf15ac4f2
chamikon-loci <chamikon.ks@gmail.com>
2026-09-23 10:59:18 +0700

    Add initial pieces

--- frontend/components/Board.jsx
+++ frontend/components/Board.jsx
@@ -1,4 +1,19 @@
 import '../styles/chess.css'
+import blackRook from '../images/br.png'
+import blackBishop from '../images/bb.png'
+import blackKnight from '../images/bk.png'
+import blackQueen from '../images/bq.png'
+import blackKing from '../images/bk.png'
+
+import blackPawn from '../images/bp.png'
+
+import whiteRook from '../images/wr.png'
+import whiteBishop from '../images/wb.png'
+import whiteKnight from '../images/wk.png'
+import whiteQueen from '../images/wq.png'
+import whiteKing from '../images/wk.png'
+
+import whitePawn from '../images/wp.png'
 
 const Board = () => {
 
@@ -12,17 +27,56 @@ const Board = () => {
         return (i+j)%2 === 0 ? 'light' : 'dark'
     }
 
+    const pieces = {
+        a1: whiteRook,
+        b1: whiteKnight,
+        c1: whiteBishop,
+        d1: whiteQueen,
+        e1: whiteKing,
+        f1: whiteBishop,
+        g1: whiteKnight,
+        h1: whiteRook,
+
+        a2: whitePawn,
+        b2: whitePawn,
+        c2: whitePawn,
+        d2: whitePawn,
+        e2: whitePawn,
+        f2: whitePawn,
+        g2: whitePawn,
+        h2: whitePawn,
+
+        a7: blackPawn,
+        b7: blackPawn,
+        c7: blackPawn,
+        d7: blackPawn,
+        e7: blackPawn,
+        f7: blackPawn,
+        g7: blackPawn,
+        h7: blackPawn,
+
+        a8: blackRook,
+        b8: blackKnight,
+        c8: blackBishop,
+        d8: blackQueen,
+        e8: blackKing,
+        f8: blackBishop,
+        g8: blackKnight,
+        h8: blackRook,
+    }
+
     return (
         <div className='chess-board'>
             {row.map((r, i) => 
-                col.map((c, j) => 
-                    <button key={`${c}${r}`} 
-                        className={whatColor(i, j)} 
-                    >
-                        
-                        {c}{r}
-                    </button>
-            ))}
+                col.map((c, j) => {
+                    const position = c+r;
+                    return (
+                        <button key={position} className={whatColor(i, j)}>
+                            <img src={[pieces[position]]} className='pieces'/>
+                        </button>
+                    )
+                }))
+            }
         </div>
     )
 }
