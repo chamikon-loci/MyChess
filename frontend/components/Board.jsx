5db17bcb5522d6dc9ffe3e315f65317277064e5f
chamikon-loci <chamikon.ks@gmail.com>
2026-09-23 18:05:06 +0700

    try some move logic

--- frontend/components/Board.jsx
+++ frontend/components/Board.jsx
@@ -15,6 +15,8 @@ import whiteKing from '../images/wk.png'
 
 import whitePawn from '../images/wp.png'
 
+import { useState } from 'react'
+
 const Board = () => {
 
     const row = []
@@ -27,42 +29,85 @@ const Board = () => {
         return (i+j)%2 === 0 ? 'light' : 'dark'
     }
 
-    const pieces = {
-        a1: whiteRook,
-        b1: whiteKnight,
-        c1: whiteBishop,
-        d1: whiteQueen,
-        e1: whiteKing,
-        f1: whiteBishop,
-        g1: whiteKnight,
-        h1: whiteRook,
+    const [pieces, setPieces] = useState({
+        a1: whiteRook, b1: whiteKnight, c1: whiteBishop, d1: whiteQueen, e1: whiteKing, f1: whiteBishop, g1: whiteKnight, h1: whiteRook,
+        a2: whitePawn, b2: whitePawn, c2: whitePawn, d2: whitePawn, e2: whitePawn, f2: whitePawn, g2: whitePawn, h2: whitePawn,
+        a3: '', b3: '', c3: '', d3: '', e3: '', f3: '', g3: '', h3: '',
+        a4: '', b4: '', c4: '', d4: '', e4: '', f4: '', g4: '', h4: '',
+        a5: '', b5: '', c5: '', d5: '', e5: '', f5: '', g5: '', h5: '',
+        a6: '', b6: '', c6: '', d6: '', e6: '', f6: '', g6: '', h6: '', 
+        a7: blackPawn, b7: blackPawn, c7: blackPawn, d7: blackPawn, e7: blackPawn, f7: blackPawn, g7: blackPawn, h7: blackPawn,
+        a8: blackRook, b8: blackKnight, c8: blackBishop, d8: blackQueen, e8: blackKing, f8: blackBishop, g8: blackKnight, h8: blackRook,
+    })
 
-        a2: whitePawn,
-        b2: whitePawn,
-        c2: whitePawn,
-        d2: whitePawn,
-        e2: whitePawn,
-        f2: whitePawn,
-        g2: whitePawn,
-        h2: whitePawn,
+    const [select, setSelect] = useState(null)
+    const [prevPos, setPrevPos] = useState('')
 
-        a7: blackPawn,
-        b7: blackPawn,
-        c7: blackPawn,
-        d7: blackPawn,
-        e7: blackPawn,
-        f7: blackPawn,
-        g7: blackPawn,
-        h7: blackPawn,
+    function movePiece(position) {    
+        if(select === null) {
+            setPrevPos(position)
+            console.log(prevPos)
+            if(pieces[position] === '') {
+                return;
+            }
+            setSelect(pieces[position])
+            setPieces((prev) => ({...prev, [position]: ''}))
+            return;
+        }
+        if(pieces[position] === '') {
+            
+            if(select === '/images/wp.png') {
+                if(prevPos.endsWith('2')) {
+                    if(!position.endsWith('3') && !position.endsWith('4')) {
+                        return;
+                    }
+                    if(position[0] !== prevPos[0]) {
+                        
+                        return;
+                    }
+                    setPieces((prev) => ({...prev, [position]: select}))
+                    setSelect(null)
 
-        a8: blackRook,
-        b8: blackKnight,
-        c8: blackBishop,
-        d8: blackQueen,
-        e8: blackKing,
-        f8: blackBishop,
-        g8: blackKnight,
-        h8: blackRook,
+                    setPrevPos(position)
+                    console.log(prevPos)
+                } else {
+                    if(position[0] !== prevPos[0]) {
+                        return;
+                    }
+                    if(position[1]-prevPos[1] === 1){
+                        setPieces((prev) => ({...prev, [position]: select}))
+                        setSelect(null)
+                    } 
+                }
+            } 
+
+            if(select === '/images/wr.png') {
+                if(position[0] !== prevPos[0] && position[1] !== prevPos[1]) {
+                    return;
+                }
+                setPieces((prev) => ({...prev, [position]: select}))
+                setSelect(null)
+            }
+
+            if(select === '/images/wn.png') {
+                
+            }
+
+            if(select === '/images/wb.png') {
+                
+            }
+
+            if(select === '/images/wq.png') {
+                
+            }
+
+            if(select === '/images/wp.png') {
+                
+            }
+            
+            //setPieces((prev) => ({...prev, [position]: select}))
+            //setSelect(null)
+        }
     }
 
     return (
@@ -71,8 +116,13 @@ const Board = () => {
                 col.map((c, j) => {
                     const position = c+r;
                     return (
-                        <button key={position} className={whatColor(i, j)}>
-                            <img src={pieces[position]} className='pieces'/>
+                        <button key={position} className={whatColor(i, j)} onClick={() => movePiece(position)}>
+                            {pieces[position] && (
+                                <img
+                                    src={pieces[position]}
+                                    className='pieces'
+                                />
+                            )}
                         </button>
                     )
                 }))
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-const socket = io.connect('http://localhost:3001')
+//const socket = io.connect('http://localhost:3001')
 
 import Chess from '../pages/Chess'
 
