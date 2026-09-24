7a9746ac021bb4a33e84b9aaf1fe65749e3ae310
chamikon-loci <chamikon.ks@gmail.com>
2026-09-24 17:42:41 +0700

    Add move some basic move rules

--- frontend/components/Board.jsx
+++ frontend/components/Board.jsx
@@ -43,15 +43,90 @@ const Board = () => {
     const [select, setSelect] = useState(null)
     const [prevPos, setPrevPos] = useState('')
 
+    function isClearPath(from, to) {
+        const fromCol = from.charCodeAt(0)
+        const fromRow = Number(from[1])
+
+        const toCol = to.charCodeAt(0)
+        const toRow = Number(to[1])
+
+        let colMove = Math.sign(toCol - fromCol)
+        let rowMove = Math.sign(toRow - fromRow)
+
+        let currentCol = fromCol + colMove
+        let currentRow = fromRow + rowMove
+
+        while(currentCol !== toCol || currentRow !== toRow) {
+
+            const tempPos = String.fromCharCode(currentCol) + currentRow
+            console.log(tempPos)
+            if(pieces[tempPos] !== '') 
+                return false
+
+            currentCol += colMove
+            currentRow += rowMove
+        }
+
+        return true
+    }
+
+    function isValidKnightMove(from, to) {
+
+        const fromCol = from.charCodeAt(0)
+        const fromRow = Number(from[1])
+
+        const toCol = to.charCodeAt(0)
+        const toRow = Number(to[1])
+
+        if(Math.abs(toCol - fromCol) === 1 && Math.abs(toRow - fromRow) === 2)
+            return true
+        if(Math.abs(toCol - fromCol) === 2 && Math.abs(toRow - fromRow) === 1)
+            return true
+
+        return false
+        
+    }
+
+    function isValidKingMove(from, to) {
+        const fromCol = from.charCodeAt(0)
+        const fromRow = Number(from[1])
+        
+        const toCol = to.charCodeAt(0)
+        const toRow = Number(to[1])
+        
+        if(Math.abs(toCol - fromCol) <= 1 && Math.abs(toRow - fromRow) <= 1 && 
+        (Math.abs(toCol - fromCol) !== 0 || Math.abs(toRow - fromRow) !== 0))
+            return true
+
+        return false
+    }
+
+    function isValidBishopMove(from, to) {
+
+        const fromCol = from.charCodeAt(0)
+        const fromRow = Number(from[1])
+
+        const toCol = to.charCodeAt(0)
+        const toRow = Number(to[1])
+
+        
+        if(!isClearPath(from, to)) return false
+
+        if(!(Math.abs(toCol - fromCol) === Math.abs(toRow - fromRow))) {
+            return false
+        }
+
+        return true
+    }
+
     function movePiece(position) {    
         if(select === null) {
             setPrevPos(position)
-            console.log(prevPos)
+            console.log('prevPos when select is null: ', prevPos)
             if(pieces[position] === '') {
                 return;
             }
             setSelect(pieces[position])
-            setPieces((prev) => ({...prev, [position]: ''}))
             return;
         }
         if(pieces[position] === '') {
@@ -59,22 +134,25 @@ const Board = () => {
             if(select === '/images/wp.png') {
                 if(prevPos.endsWith('2')) {
                     if(!position.endsWith('3') && !position.endsWith('4')) {
+                        setPrevPos('')
                         return;
                     }
                     if(position[0] !== prevPos[0]) {
-                        
+                        setPrevPos('')
                         return;
                     }
+                    setPieces((prev) => ({...prev, [prevPos]: ''}))
                     setPieces((prev) => ({...prev, [position]: select}))
                     setSelect(null)
 
                     setPrevPos(position)
-                    console.log(prevPos)
+                    console.log('prevPos of pawn move: ', prevPos)
                 } else {
                     if(position[0] !== prevPos[0]) {
                         return;
                     }
                     if(position[1]-prevPos[1] === 1){
+                        setPieces((prev) => ({...prev, [prevPos]: ''}))
                         setPieces((prev) => ({...prev, [position]: select}))
                         setSelect(null)
                     } 
@@ -82,31 +160,52 @@ const Board = () => {
             } 
 
             if(select === '/images/wr.png') {
-                if(position[0] !== prevPos[0] && position[1] !== prevPos[1]) {
-                    return;
+                if(isClearPath(prevPos, position)) {
+                    if(position[0] !== prevPos[0] && position[1] !== prevPos[1]) {
+                        return;
+                    }
+                    setPieces((prev) => ({...prev, [prevPos]: ''}))
+                    setPieces((prev) => ({...prev, [position]: select}))
+                    setSelect(null)
+                } else {
+                    setSelect(null)
                 }
-                setPieces((prev) => ({...prev, [position]: select}))
-                setSelect(null)
             }
 
             if(select === '/images/wn.png') {
-                
+                if(isValidKnightMove(prevPos, position)) {
+                    setPieces((prev) => ({...prev, [prevPos]: ''}))
+                    setPieces((prev) => ({...prev, [position]: select}))
+                    setSelect(null)
+                } else {
+                    setSelect(null)
+                }
+
             }
 
             if(select === '/images/wb.png') {
-                
+                if(isValidBishopMove(prevPos, position)) {
+                    setPieces((prev) => ({...prev, [prevPos]: ''}))
+                    setPieces((prev) => ({...prev, [position]: select}))
+                    setSelect(null)
+                } else {
+                    setSelect(null)
+                }
             }
 
             if(select === '/images/wq.png') {
                 
             }
 
-            if(select === '/images/wp.png') {
-                
+            if(select === '/images/wk.png') {
+                if(isValidKingMove(prevPos, position)) {
+                    setPieces((prev) => ({...prev, [prevPos]: ''}))
+                    setPieces((prev) => ({...prev, [position]: select}))
+                    setSelect(null)
+                } else {
+                    setSelect(null)
+                }
             }
-            
-            //setPieces((prev) => ({...prev, [position]: select}))
-            //setSelect(null)
         }
     }
 
